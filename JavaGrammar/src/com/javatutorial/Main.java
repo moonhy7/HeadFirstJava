d11bb18dca586598dc788af24be2173b0e396f7d
moonhy7 <moonhy7@naver.com>
2022-01-24 10:45:33 +0900

    22-01-24

---
+++ JavaGrammar/src/com/javatutorial/Main.java
@@ -0,0 +1,9 @@
+package com.javatutorial;
+
+public class Main {
+	public static void main(String[] args) {
+		Pair<String, Integer> p1 = new OrderedPair<>("Even", 8);
+		Pair<String, String> p2 = new OrderedPair<>("hello", "world");
+	
+	}
+}
---
+++ JavaGrammar/src/com/javatutorial/Pair.java
@@ -0,0 +1,6 @@
+package com.javatutorial;
+
+public interface Pair<K, V> {
+    public K getKey();
+    public V getValue();
+}
